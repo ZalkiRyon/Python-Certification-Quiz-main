1697c9cdc41b0693376c521b31b80ce1fbe53bc5
Ryon00 <se.valdiviae@duocuc.cl>
2024-07-03 09:51:47 -0400

    agregado lista2 con problemas

--- import random.py
+++ import random.py
@@ -1,6 +1,7 @@
 import random
 
 trabajadores = ["Juan Pérez","María García","Carlos López","Ana Martínez","Pedro Rodríguez","Laura Hernández","Miguel Sánchez","Isabel Gómez","Francisco Díaz","Elena Fernández"]
+trabajadores2 = ["Juan Pérez”,”María García”,”Carlos López”,”Ana Martínez”,”Pedro Rodríguez”,”Laura Hernández”,”Miguel Sánchez”,”Isabel Gómez”,”Francisco Díaz”,”Elena Fernández"]
 
 sueldos = []
 afp = []
